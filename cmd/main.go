c3413afb8e07a9b3d0dedf20175a2e019c60e0d5
whyy <icarovsilva1@gmail.com>
2023-11-30 09:26:02 -0300

    fix: load env

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"errors"
-	"log"
-
 	"github.com/IcaroSilvaFK/free-code-source-back/cmd/routes"
 	"github.com/IcaroSilvaFK/free-code-source-back/infra/validators"
 	"github.com/go-playground/validator"
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
-
-	if err := godotenv.Load(); !errors.Is(err, nil) {
-		log.Fatal("Error loading .env file")
-	}
+	//TODO remove comment to enable .env in development
+	// if err := godotenv.Load(); !errors.Is(err, nil) {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	e := echo.New()
 
